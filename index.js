ca79510f03bcd372a74a4cce8c6a9e5985ab2c4a
Stephen Corry <stevemcorry@gmail.com>
2016-11-16 15:31:28 -0700

    done

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
+var massive = require('massive');
+var connectionString = "postgres://postgres:yeah1buddy@localhost/sandbox";
+var massiveInstance = massive.connectSync({connectionString : connectionString});
 var app = module.exports = express();
+var controller = require('./controller.js');
 app.use(bodyParser.json());
 app.use(cors());
+app.set('db',massiveInstance);
+var db = app.get('db');
+
+// db.new_plane(function(err, planes){
+//     console.log(err, "plane added");
+// });
+
+controller.getPlanes();
 
 app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
-
+  console.log("Successfully listening on : 3000");
+});
